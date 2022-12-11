f786777b64f9deb6d05d5572427fd3762774e346
Raul Janari <rdjanari1707@gmail.com>
2022-12-11 21:28:03 +0100

    Codewars - EvenOrOdd(CS, 8kyu)

---
+++ Codewars/Csharp/8kyu/EvenOrOdd.cs
@@ -0,0 +1,14 @@
+class EvenOrOdd
+{
+    public string Solution(int number)
+    {
+        if (number % 2 == 0)
+        {
+            return "Even";
+        }
+        else
+        {
+            return "Odd";
+        }
+    }
+}
