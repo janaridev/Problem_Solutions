0cb29ad993ddc1bb39be6e34a372da71b959e17c
Raul Janari <rdjanari1707@gmail.com>
2022-12-24 18:00:31 +0100

    LeetCode_Easy - IsPalindrome (JS)

---
+++ LeetCode/LeetCode_Easy/JS/IsPalindrome.js
@@ -0,0 +1,7 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    return false ? x < 0 : true ? x == x.toString().split('').reverse().join('') : x < 10 && x >= 0 
+};
