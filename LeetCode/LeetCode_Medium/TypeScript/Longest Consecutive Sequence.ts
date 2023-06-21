9dffad7b327e51c3c746ee43c7d8fe5ef7afa2fc
Raul Janari <rdjanari1707@gmail.com>
2023-06-21 07:23:37 +0200

    LeetCode_Medium - Longest Consecutive Sequence(TS)

---
+++ LeetCode/LeetCode_Medium/TypeScript/Longest Consecutive Sequence.ts
@@ -0,0 +1,27 @@
+function longestConsecutive(nums: number[]): number {
+    const consecutiveSequence: Array<number> = [];
+
+    nums.sort((a: number, b: number) => a - b);
+
+    let length = 0;
+    let i = 0;
+    for (let n = 0; n < nums.length; n++) {
+        if (n === 0) {
+            i = nums[n] - 1;
+        }
+
+        if (i === nums[n]) continue;
+
+        if (i + 1 !== nums[n]) {
+            i = nums[n];
+            consecutiveSequence.push(length);
+            length = 1;
+            continue;
+        }
+        i = nums[n];
+        length += 1;
+    }
+    consecutiveSequence.push(length);
+
+    return Math.max(...consecutiveSequence);
+}
