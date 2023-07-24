034201bd914f299d841877eca0349d831a3d8fb6
Raul Janari <rdjanari1707@gmail.com>
2023-07-24 06:21:57 +0200

    LeetCode_Easy - Reverse Linked List (TS)

---
+++ LeetCode/LeetCode_Easy/TypeScript/ReverseLinkedList.ts
@@ -0,0 +1,21 @@
+// Definition for singly-linked list.
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function reverseList(head: ListNode | null): ListNode | null {
+  let prev: ListNode | null = null;
+  let curr: ListNode | null = head;
+  while (curr !== null) {
+    const next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  return prev;
+}
